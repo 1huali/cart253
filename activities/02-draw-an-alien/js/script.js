e6ab1cd7277f1584f814babd55c6939989d3d4b4
1huali <wanhuali2000@gmail.com>
2021-09-14 21:27:56 -0400

    Done drawing the head and body.

--- activities/02-draw-an-alien/js/script.js
+++ activities/02-draw-an-alien/js/script.js
@@ -23,9 +23,25 @@ Description of setup
 function setup() {
   createCanvas (640,480);
   background (255,192,203);
+// Drawing a body
   noStroke();
   fill(200,200,200);
-  ellipse();
+  ellipse(320,480,350,200)
+// Drawing a head
+  fill(139,139,139);
+  ellipse(320,240,230,440);
+// Drawing the eyes
+  fill(0,0,0);
+  ellipse(270,160,50,200);
+  ellipse(365,160,50,200);
+// Drawing the nostrills
+  ellipse(315,260,6,9);
+  ellipse(330,260,6,9);
+// Drawing the mouth
+  stroke(1);
+  strokeWeight (2);
+  rectMode(CENTER);
+  rect(320,350,60,30,15);
 
 }
 
