9c3efe40a1356d25d0beb17baab9fc076e90cd88
1huali <wanhuali2000@gmail.com>
2021-09-15 16:17:48 -0400

    Personalized alien.

--- activities/02-draw-an-alien/js/script.js
+++ activities/02-draw-an-alien/js/script.js
@@ -22,25 +22,34 @@ Description of setup
 */
 function setup() {
   createCanvas (640,480);
-  background (255,192,203);
+  background(242, 7, 47);
+  //background (255,192,203);
+
 // Drawing a body
   noStroke();
-  fill(200,200,200);
+  //fill(200,200,200);
+  fill(252, 141, 160)
   ellipse(320,480,350,200)
+
 // Drawing a head
-  fill(139,139,139);
-  ellipse(320,240,290,440);
+//  fill(139,139,139);
+  fill(255,192,203);
+  ellipse(320,240,260,420);
+
 // Drawing the eyes
   fill(0,0,0);
-  ellipse(270,190,100,250);
-  ellipse(375,190,100,250);
+  ellipse(250,220,100,250);
+  ellipse(390,220,100,250);
+
 // Drawing the nostrills
   ellipse(315,330,6,9);
   ellipse(330,330,6,9);
+
 // Drawing the mouth
   stroke(1);
   strokeWeight (2);
-  fill(101);
+  noStroke();
+  fill(217,80,101);
   rectMode(CENTER);
   rect(320,376,90,15,15);
 
