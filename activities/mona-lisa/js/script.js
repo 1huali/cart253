715cc5a1c9565ae730c724187039548865790290
1huali <wanhuali2000@gmail.com>
2021-09-16 15:14:13 -0400

    Update script.js

--- activities/mona-lisa/js/script.js
+++ activities/mona-lisa/js/script.js
@@ -22,7 +22,40 @@ Description of setup
 */
 function setup() {
   createCanvas(400,600);
-  background();
+  background(123, 163, 121);
+
+  //eyes
+  fill(56, 31, 13)
+  ellipse(140,150,20,10)
+
+
+  //hair
+  fill(56, 31, 13);
+  noStroke();
+  ellipse(210,155,180,210);
+
+
+  //draw face
+  fill(222, 216, 171);
+  noStroke();
+  ellipse(200,150,120,180);
+  rectMode(CENTER);
+  rect(200,180,50,20,15);
+  //
+  //visage elements
+
+  //
+
+  // mouth
+    // fill(222, 216, 171);
+    // noStroke();
+    // rectMode(CENTER);
+    // rect(200,180,50,20,15);
+
+//mouth arc
+noFill()
+stroke(1)
+arc(200, 155, 70, 70, radians(70), radians(100));
 
 
 }
