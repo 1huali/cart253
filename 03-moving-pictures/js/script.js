b92367b12310d40bb06a824a8fbb032a9adce4b5
1huali <wanhuali2000@gmail.com>
2021-09-22 21:48:19 -0400

    Finalized and refined.

--- 03-moving-pictures/js/script.js
+++ 03-moving-pictures/js/script.js
@@ -22,8 +22,9 @@ let circle1= {
   y:350,
   size: 250,
   fill: 0,
-  alpha: 200
-  // speed:1
+  alpha: 200,
+  speed:1,
+  growthRate:0.9
 }
 let circle2= {
   x: 1000,
@@ -31,8 +32,8 @@ let circle2= {
   size: 100,
   fill: 0,
   alpha: 100,
-  // speed: 1
-
+  speed: -1,
+  growthRate:0.9
 }
 
 /**
@@ -59,14 +60,15 @@ function draw() {
 // bg
   background(bg.r,bg.b,bg.g);
   bg.r = bg.r+1;
+  bg.r = map(bg.r,100, width,100,255);
 
   // circle 1
   ellipse(circle1.x,circle1.y,circle1.size);
   fill(circle1.fill, circle1.alpha);
   noStroke();
-  circle1.x= circle1.x+1;
+  circle1.x= circle1.x+circle1.speed;
   circle1.x= constrain(circle1.x,0,500);
-  circle1.size= circle1.size+0.25;
+  circle1.size= circle1.size+circle2.growthRate;
   circle1.size= constrain(circle1.size,0,height);
   //
   // circle1.x= circle1.x+circle1.speed;
@@ -77,10 +79,10 @@ function draw() {
   ellipse(circle2.x,circle2.y,circle2.size);
   noStroke();
   fill(circle2.fill, circle2.alpha);
-  circle2.x= circle2.x-1
+  circle2.x= circle2.x+circle2.speed;
   circle2.x= constrain(circle2.x,500,1000);
-  circle2.size= circle2.size+0.25;
-  circle2.size= constrain(circle2.size,0,height);
+  circle2.size= circle2.size+circle2.growthRate;
+  circle2.size= constrain(circle2.size,0,circle1.size*0.75);
 
   // fill(255, 183, 163);
 
