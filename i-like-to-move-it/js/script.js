c7d998d660f9d7dd6a7456787abee77d2add62bb
1huali <wanhuali2000@gmail.com>
2021-09-23 16:07:08 -0400

    Creating circles.

--- i-like-to-move-it/js/script.js
+++ i-like-to-move-it/js/script.js
@@ -2,18 +2,18 @@
 I like to move it
 Wan Hua Li
 
+Exercice including (décrire les mvtmts)
+representation of a close-up of cellulosis of 
 This is a template. You must fill in the title,
 author, and this description to match your project!
 */
 
 "use strict";
 
-let rectangle={
-  x:0,
-  y:250,
-  size: 25,
-  vx:10,
-  vy:10
+// circle
+let circle1={
+
+
 }
 
 /**
@@ -28,10 +28,9 @@ function preload() {
 Description of setup
 */
 function setup() {
-  createCanvas(500,500);
+  createCanvas(1000,700);
   background(0);
 
-  random()
 }
 
 
@@ -39,13 +38,37 @@ function setup() {
 Description of draw()
 */
 function draw() {
-rectangle.x= rectangle.x+rectangle.vx;
-rectangle.y= rectangle.x+rectangle.vy;
+
+// novas, grow and shrinks
+ellipse(140,150,100);
+ellipse(140,150,50);
+
+ellipse(500,400,100);
+ellipse(500,400,70);
+
+ellipse(500,400,100);
+ellipse(500,400,50);
+
+ellipse(120,520,100);
+ellipse(120,520,60);
+
+ellipse(810,350,100);
+ellipse(810,350,50);
 
 
-rectMode(CENTER);
-noFill();
-stroke(255)
-rectangle(rectangle.x,project.y,rectangle.size, rectangle.size);
+fill(247, 163, 210)
+
+// stars, turn
+ellipse(300,200,100);
+ellipse(700,612,200);
+ellipse(750,175,200);
+ellipse(140,612,60);
+
+fill(247, 10, 210)
+
+// dust - moves quick like a heart-beat
+ellipse(500,125,70);
+ellipse(250,525,70);
+
 
 }
