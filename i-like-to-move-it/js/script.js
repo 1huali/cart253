4e7305b6e056522e389dbe9e7d69d892268b052c
1huali <wanhuali2000@gmail.com>
2021-09-23 18:29:07 -0400

    Ex.1, troubleshooting
    - circle get bigger w mouseX.

--- i-like-to-move-it/js/script.js
+++ i-like-to-move-it/js/script.js
@@ -11,6 +11,13 @@ author, and this description to match your project!
 "use strict";
 // SETTING OBJECTS N STARSZSZSZSZSZSZSZSZSZS
 
+// bg
+let backgroundColor={
+  r:155,
+  g:155,
+  b:155
+};
+
 // dust/foam - moves quick like a heart-beat
 // ellipse(500,125,70);
 // ellipse(250,525,70);
@@ -19,7 +26,7 @@ let foam1={
   x:500,
   y:125,
   size:70,
-  speed:1,
+  speed:10,
   fill:1
 }
 
@@ -27,7 +34,7 @@ let foam2={
   x:250,
   y:525,
   size:70,
-  speed:1,
+  speed:10,
   fill:1
 }
 
@@ -46,18 +53,13 @@ let foam2={
 //
 // ellipse(810,350,100);
 // ellipse(810,350,50);
-let nova1={
-x:500,
-y:400,
-size:100,
-fill:0
-}
+
 let core1={
   x:500,
   y:400,
   size:70,
-  alpha:200,
-  fill:255
+  alpha:100,
+  fill:2
 }
 
 let nova2={
@@ -146,14 +148,22 @@ Description of setup
 */
 function setup() {
   createCanvas(1000,700);
-  background(196, 181, 190);
+  noStroke();
 }
 
 
 /**
 Description of draw()
 */
+
 function draw() {
+  backgroundColor.r= map(mouseX,0,width,255,247);
+  backgroundColor.g= map(mouseX,0,width,255,62);
+  backgroundColor.b= map(mouseX,0,width,255,117);
+
+
+  background(backgroundColor.r,backgroundColor.g,backgroundColor.b);
+
 // stars appears
   ellipse(star1.x,star1.y,star1.size);
   ellipse(star2.x,star2.y,star2.size);
@@ -162,14 +172,27 @@ function draw() {
   noStroke();
   ellipse(foam1.x,foam1.y,foam1.size);
   ellipse(foam2.x,foam2.y,foam2.size);
+
+  let nova1={
+  x:500,
+  y:400,
+  size:0,
+  fill:0
+  }
+
+  nova1.size= nova1.size+mouseX;
+  nova1.size= constrain(nova1.size, 0,width);
   ellipse(nova1.x,nova1.y,nova1.size);
   ellipse(nova2.x,nova2.y,nova2.size);
   ellipse(nova3.x,nova3.y,nova3.size);
   ellipse(nova4.x,nova4.y,nova4.size);
-  ellipse(core1.x,core1.y,core1.size,core1.alpha);
-  ellipse(core2.x,core2.y,core2.size,core2.alpha);
-  ellipse(core3.x,core3.y,core3.size,core3.alpha);
-  ellipse(core4.x,core4.y,core4.size,core4.alpha);
+  // push();
+  fill(251,11,234,core1.alpha);
+  ellipse(core1.x,core1.y,core1.size);
+  // pop();
+  ellipse(core2.x,core2.y,core2.size);
+  ellipse(core3.x,core3.y,core3.size);
+  ellipse(core4.x,core4.y,core4.size);
   // color appears
   fill(star1.fill);
   fill(star2.fill);
@@ -190,8 +213,12 @@ function draw() {
   fill(foam2.fill);
 
 
-// code for heartbeat mvmt
+code for foam heartbeat mvmt
+foam1.speed= random (-2,2);
+foam1.x = foam1.x+ foam1.speed;
 
+foam2.speed= random (-2,2);
+foam2.x = foam2.x+ foam2.speed;
 
 
 }
