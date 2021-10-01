8fe2cf7c621f443d6d53dc1e8c6f4c1d9e1bc491
1huali <wanhuali2000@gmail.com>
2021-09-30 20:45:15 -0400

    Preset covid19.

--- 04-dodging-covid-19/js/script.js
+++ 04-dodging-covid-19/js/script.js
@@ -1,5 +1,5 @@
 /**
-Dodge'Em
+Dodge'Em : Venus Fly
 Wan Hua Li
 
 Better dodge those bad thoughts cos then might as well catch covid-19 init??
@@ -11,8 +11,18 @@ Exercice to put in practice mvmt variables, visuals, and conditionals.
 
 let backgroundShade=0;
 
+// called covid-19 - to dodge
+let flower ={
+  x:0, y:250, size: 100, vx:0, vy:0, speed:3,
+  fill: (){
+    r:255,
+    g:0,
+    b:0
+  }
+}
+
 // player's cursor
-let nrj ={
+let fly ={
   x:0, // will mouseX
   y:0, // will mouseY
   size: 100,
@@ -20,10 +30,7 @@ let nrj ={
   vy:0,
   fill: 255
 }
-// danger to dodge
-let danger ={
-  x:0, y:0, size: 100,speed:0.05, fill: 150
-}
+
 
 /**
 maybe medias???
@@ -47,28 +54,4 @@ Description of draw()
 function draw() {
   background(backgroundShade);
 
-  // danger bouncing
-  fill(danger.fill);
-  danger.x= danger.x+danger.speed;
-  danger.y=danger.y+danger.speed;
-
-if (danger.x> width){
-  danger.speed=-danger.speed;
-}
-else if (danger.x<0){
-  danger.speed=danger.speed;
-}
-
-if (danger.y>width){
-  danger.speed=-danger.speed;
-}
-else if (danger.y<0){
-  danger.speed=danger.speed;
-}
-
-  danger.x = random(0,width);
-  danger.y= random(0,height);
-
-
-  ellipse (danger.x, danger.y,danger.size);
 }
