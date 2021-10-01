7eb52c227db1aa8b9af45da7599c2f202a824316
1huali <wanhuali2000@gmail.com>
2021-09-30 21:10:45 -0400

    Tackled Covid19.

--- 04-dodging-covid-19/js/script.js
+++ 04-dodging-covid-19/js/script.js
@@ -12,14 +12,14 @@ Exercice to put in practice mvmt variables, visuals, and conditionals.
 let backgroundShade=0;
 
 // called covid-19 - to dodge
-let flower ={
+let covid19 ={
   x:0, y:250, size: 100, vx:0, vy:0, speed:3,
-  fill: (){
+  fill: {
     r:255,
     g:0,
     b:0
   }
-}
+};
 
 // player's cursor
 let fly ={
@@ -45,6 +45,9 @@ Description of setup
 */
 function setup() {
   createCanvas (windowWidth,windowHeight);
+
+  covid19.y = random(0,height);
+  covid19.vx=covid19.speed;
 }
 
 
@@ -54,4 +57,15 @@ Description of draw()
 function draw() {
   background(backgroundShade);
 
+if(covid19.x > width){
+  covid19.x=-covid19.speed;
+  covid19.y= random(0,height);
 }
+
+  covid19.x=covid19.x+covid19.vx;
+  covid19.y= covid19.y+covid19.vy;
+
+  fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
+  ellipse (covid19.x, covid19.y, covid19.size);
+
+  }
