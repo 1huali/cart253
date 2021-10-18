9f0c8d908d8034b20a01f81cdd022354f950d6da
1huali <wanhuali2000@gmail.com>
2021-10-18 19:26:03 -0400

    disk2 has gradient!!!

--- vj board/js/script.js
+++ vj board/js/script.js
@@ -15,12 +15,21 @@ let disk1 = {
   x:undefined,
   y:undefined,
   size:undefined,
+  h1:100,
+  s1:100,
+  b1:100,
+  rev1:1,
+}
+
+let disk2 = {
+  x:undefined,
+  y:undefined,
+  size:undefined,
   h:100,
   s:100,
   b:100,
   rev:1,
 }
-
 
 // // properties
 // // sliders
@@ -42,14 +51,26 @@ Description of setup
 */
 function setup() {
   createCanvas(1000, 600);
-  disk1.x= width/2;
+
+
+  disk1.x= width/4;
   disk1.y=height/2;
   disk1.size= 200;
   background(bg);
   noStroke();
   ellipseMode(RADIUS);
   colorMode(HSB, 360, 100, 100);
+  frameRate (60);
 
+
+push()
+disk2.x= 3*width/4;
+disk2.y=height/2;
+disk2.size= 200;
+noStroke();
+// ellipseMode(RADIUS);
+// colorMode(HSB, 360, 100, 100);
+pop();
 
 }
 
@@ -59,54 +80,107 @@ Description of draw()
 */
 function draw() {
   displayDisk1();
+  displayDisk2();
 
   for (let x = 0; x <= width; x += disk1.size) {
       drawGradient(disk1.x, height / 2);
     }
 
+    for (let x = 0; x <= width; x += disk2.size) {
+        drawGradient1(disk2.x, height / 2);
+      }
   // properties();
   bright();
   colorhue();
   sat();
+
+  bright1();
+  colorhue1();
+  sat1();
+
 }
 
-function drawGradient(x, y) {
+
+
+function drawGradient1(x, y) {
   let radius = disk1.size;
-  let h = disk1.h;
+  let h = disk1.h1;
   for (let r = radius; r > 0; --r) {
-    fill(h, disk1.s, disk1.b);
+    fill(h, disk1.s1, disk1.b1);
     ellipse(disk1.x, disk1.y, r);
     h = (h + 0.6) % 360;
   }
 }
 
+  function drawGradient(x, y) {
+    let radius = disk2.size;
+    let h = disk2.h;
+    for (let r = radius; r > 0; --r) {
+      fill(h, disk2.s, disk2.b);
+      ellipse(disk2.x, disk2.y, r);
+      h = (h + 0.6) % 360;
+    }
+}
+
+
 
 // VARIATIONS
 
+//DISK1
 // sound, color
-function colorhue(){
+function colorhue1(){
   if (keyIsDown(RIGHT_ARROW)){
-    disk1.h+=1;
+    disk1.h1+=1;
   }
   else if (keyIsDown(LEFT_ARROW)){
-    disk1.h-=1;
+    disk1.h1-=1;
   }
-  constrain(disk1.h,0,360);
+  constrain(disk1.h1,0,360);
+  }
+// volume, brightness ---- brightness kidna suck, should replace w speed, and saturation
+function bright1(){
+  disk1.b1= mouseX;
+  constrain(disk1.b1,0,360);
+}
+// saturation,volume??
+function sat1() {
+  if (keyIsDown(UP_ARROW)){
+    disk2.s1+=1;
+  }
+  else if (keyIsDown(DOWN_ARROW)) {
+    disk2.s1-=1;
+  }
+  constrain(disk2.s1,0,100);
+}
+
+// DISK2
+
+function colorhue(){
+  if (keyIsDown(65)){
+    disk2.h+=1;
+  }
+  else if (keyIsDown(68)){
+    disk2.h-=1;
+  }
+  constrain(disk2.h,0,360);
+//
+
   }
 // volume, brightness ---- brightness kidna suck, should replace w speed, and saturation
 function bright(){
-  disk1.b= mouseX;
-  constrain(disk1.b,0,360);
+  disk2.b= mouseY;
+  constrain(disk2.b,0,360);
 }
+
 // saturation,volume??
 function sat() {
   if (keyIsDown(UP_ARROW)){
-    disk1.s+=1;
+    disk2.s+=1;
   }
   else if (keyIsDown(DOWN_ARROW)) {
-    disk1.s-=1;
+    disk2.s-=1;
   }
-  constrain(disk1.s,0,100);
+  constrain(disk2.s,0,100);
 }
 
 
@@ -117,4 +191,9 @@ function sat() {
 function displayDisk1() {
   fill(disk1.h,disk1.s,disk1.b);
   ellipse(disk1.x, disk1.y, disk1.size);
+}
+
+function displayDisk2() {
+  fill(disk2.h,disk2.s,disk2.b);
+  ellipse(disk2.x, disk2.y, disk2.size);
 }
