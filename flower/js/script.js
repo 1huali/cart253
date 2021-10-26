cc9709c7f5e3e147bc846659e7a4347b8731cccf
1huali <wanhuali2000@gmail.com>
2021-10-26 16:43:49 -0400

    changed code to Pippin's way
    You create objects in setup, and everything else behavioural in draw

--- flower/js/script.js
+++ flower/js/script.js
@@ -10,30 +10,51 @@ This started as an array exercice to end-up as a OOP exercice.
 let garden = {
   flowers : [],
   numFlowers: 20,
-grassColor : {
+  grassColor: {
     r:120,
     g:180,
     b:120,
   }
 }
+// let garden = {
+//   flowers : [],
+//   numFlowers: 20
+//   }
+//   // Q:why in the garden variable? and not by itself?
+//   // A:
+// let grassColor = {
+//     r:120,
+//     g:180,
+//     b:120,
+//   }
+
 /**
 Description of setup
 */
 function setup() {
 createCanvas (600,600);
+// background(grassColor.r,grassColor.g,grassColor.b);
+
+for (let i=0; i < garden.numFlowers ; i ++){
+  let flower= new Flower();
+if (i<garden.numFlowers){
+  garden.flowers.push(flower);
+}
+  // flower.display();
 
 }
 /**
 Description of draw()
 */
 function draw() {
-background(garden.grassColor.r,garden.grassColor.g,garden.grassColor.b);
+  background(garden.grassColor.r,garden.grassColor.g,garden.grassColor.b);
+  // background(grassColor.r,grassColor.g,grassColor.b);
 
+// Q: why it wasn't respecting the max numFlower
+// A:
 for (let i=0; i < garden.numFlowers ; i ++){
   let flower= new Flower();
-    let flower = garden.flowers[i];
-
   flower.display();
 }
 }
-// its not working lol
+}
