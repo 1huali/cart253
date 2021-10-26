44ae9f64bd4fbeab578b5263db5c8460ff36ba71
1huali <wanhuali2000@gmail.com>
2021-10-26 09:53:59 -0400

    createFlower function finished

--- flower/js/script.js
+++ flower/js/script.js
@@ -23,15 +23,40 @@ function setup() {
 createCanvas (600,600);
 
 }
-
-
 /**
 Description of draw()
 */
 function draw() {
 background(grassColor.r,grassColor.g,grassColor.b);
 
-for (i=0; i < numFlowers ; i ++){
-  random()
+for (i=0; i < garden.numFlowers ; i ++){
+  let flower= createFlower;
+  garden.flowers.push(flower);
 }
 }
+
+function createFlower(){
+  let flower ={
+    x: random(0,width),
+    y: random(0,height),
+    size:50,
+    petalThickness: 10,
+    stemLength:75,
+    stemThickness:10,
+    stemColor: {
+      r:50,
+      g:150,
+      b:50
+    }
+    petalColor = {
+      r:200,
+      g:50,
+      b:50,
+    }
+    centerColor = {
+      r:50,
+      g:0,
+      b:0
+    }
+    }
+    }
