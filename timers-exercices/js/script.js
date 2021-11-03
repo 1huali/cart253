b38420791b87e7a0a24e8062cf9389fbf25e1c4b
1huali <wanhuali2000@gmail.com>
2021-11-03 13:04:30 -0400

    Added Timer.

--- timers-exercices/js/script.js
+++ timers-exercices/js/script.js
@@ -12,7 +12,9 @@ let state = `title`;
 
 let circles = [];
 let numCircle = 10;
-
+let alive = true; // added on?
+let gameOverTimer=0;
+let gameLenght= 10*60; //10 secondes by 60 franes per second
 
 /**
 Description of setup
@@ -25,13 +27,15 @@ function setup() {
     circles.push(circle);
   }
   console.log(circles);
+
+
 }
 
-  function createCircle() {
-    let circle = {
-    x:random(0,width),
+function createCircle() {
+  let circle = {
+    x: random(0, width),
     y: random(0, height),
-    size: random(50,100),
+    size: random(50, 100),
   };
   return circle;
   // what does return means?
@@ -42,81 +46,103 @@ Call all the states
 */
 function draw() {
   background(0);
-if (state === `title`){
-  title();
-}
-else if (state === `game`){
-  game();
-}
-else if (state === `win`){
-  win();
-}
-else if (state === `lose`){
-  lose();
-}
+  if (state === `title`) {
+    title();
+  }
+  else if (state === `game`) {
+    game();
+  }
+  else if (state === `win`) {
+    win();
+  }
+  else if (state === `lose`) {
+    lose();
+  }
 }
 
-function title(){
+function title() {
   displayText(`Title`);
 }
 
 // dont understand this for loop here, and the display purpose
-function game(){
-for (let i=0; i < circles.length; i++) {
-// calling circles individually to be "circle" temporarly
-  let circle = circles[i];
-  // display them called-temporarly "circle"
-  displayCircle(circle);
-}
+function game() {
+  for (let i = 0; i < circles.length; i++) {
+    // calling circles individually to be "circle" temporarly
+    let circle = circles[i];
+    // display them called-temporarly "circle"
+    displayCircle(circle);
+  }
+  gameOverTimer++;
+  if (gameOverTimer>= gameLenght){
+    gameOver();
+    // how can I console.log the gameLenght?
+
+  }
 }
 
 // where in the program did we assign circle to be a circle in the array?
 // A: line 68. the variable is teleported and can change name but still hold properties.
-function displayCircle(circle){
+function displayCircle(circle) {
+if (circles.alive=true){
   push();
-  fill(255,0,0);
+  fill(255, 0, 0);
   noStroke();
   ellipse(circle.x, circle.y, circle.size);
   console.log(circle.x)
   pop();
 }
-
-function mousePressed(){
-if (state=== `title`){
-  state = `game`;
-}
-if (state=== `game`){
-  checkCircleClick();
-}
 }
 
-function checkCircleClick(){
-  for (let i = 0; i < circles.length ; i ++){
-    //Q:what does that do? Do we need this part to make splice work?
-    let circle = circles[i];
-    // Q: order matters or not?
-    let d = dist ( mouseX,mouseY,circle.x, circle.y)
-    if (d < circle.size/2){
-      circle.splice(i,1);
-      // Q: breaks what?
-      break;
-    }
+function mousePressed() {
+  if (state === `title`) {
+    state = `game`;
+  }
+  if (state === `game`) {
+    checkCircleClick();
   }
 }
 
-function win(){
+function checkCircleClick() {
+  for (let i = 0; i < circles.length; i++) {
+    //Q:what does that do? Do we need this part to make splice work?
+    let circle = circles[i];
+    // Q: order matters in dist or not?
+    // A: non
+    let d = dist(mouseX, mouseY, circle.x, circle.y)
+    if (d < circle.size / 2) {
+      // how can I console log if the circles are clicked/left or not?
+      circle.splice(i, 1);
+      circle.alive= false;  // added on?
+      // Q: breaks what?
+      // A: the loop
+      break;
+    }
+    // how can I console.log the array?
+  }
+}
+
+function win() {
   displayText(`Yay win`);
 }
 
-function lose(){
+function lose() {
   displayText(`U lose`);
 }
 
-function displayText(message){
+function gameOver(){
+  if (circles.lenght === 0){
+    state =`win`
+  }
+  else {
+    state = `lose`;
+  }
+}
+
+function displayText(message) {
   push();
   fill(255);
-  textAlign(CENTER,CENTER);
+  textAlign(CENTER, CENTER);
   textSize(32);
-  text(message,width/2, height/2);
+  text(message, width/2, height/2);
   pop();
 }
