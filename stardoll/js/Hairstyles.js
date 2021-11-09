bb63352daf979af44e4e08354fcc611a49d4ac97
1huali <wanhuali2000@gmail.com>
2021-11-09 18:32:43 -0500

    Update
    - draggable elements fixed : single element is dragged
    - next : add state

--- stardoll/js/Hairstyles.js
+++ stardoll/js/Hairstyles.js
@@ -1,39 +1,46 @@
 class Hairstyles
 {
-  constructor(x,y,r,g,b) {
+  constructor(x,y,img) {
     this.x=x;
     this.y=y;
-    this.width=150;
-    this.height=50;
-    this.fill ={
-      this.r=r;
-      this.g=g;
-      this.b=b;
+    this.img=img;
+    this.w=this.img.width/3;
+    this.h=this.img.height/3;
+    console.log(this.img.width)
+    this.monoClick = false;
+    // this.width=150;
+    // this.height=50;
+    // this.fill ={
+    //   this.r=r;
+    //   this.g=g;
+    //   this.b=b;
     }
 
 
 
 drag(){
-  if(mouseIsPressed){
+  if(mouseIsPressed && (hairClicked=== false || this.monoClick === true)) {
     let d= dist(mouseX, mouseY, this.x,this.y)
     console.log("mouse is being pressed")
-    if (d < this.width){
-    // console.log("hair is being pressed")
+    if (d < this.w){
+      hairClicked = true;
+      this.monoClick = true;
+    console.log("hair is being pressed")
       this.x= mouseX;
       this.y= mouseY;
     }
   }
 }
 
-  function displayHipBox(){
+  displayHipBox(){
   // tbd
   }
 
-  function displayHair (){
-  // tbd
-  }
-
-  function checkHairPosition(){
-// unactivated, but tbh if anything
-  }
+  displayHair (){
+image(this.img, this.x,this.y,this.w,this.h);
+imageMode(CENTER);
+}
+  //
+  // checkDrag(){
+  //   }
 } //end class
--- stardoll/js/script.js
+++ stardoll/js/script.js
@@ -12,34 +12,29 @@ let doll ={
   size:150,
 }
 
-let hipBox ={
-  x: 600,
-  y:120,
-  width:150,
-  height:50
-}
+// let hipBox ={
+//   x: 600,
+//   y:120,
+//   width:150,
+//   height:50
+// }
 
 let hairstyles = [];
-let hairstylesNum=2;
-let hair1 = new Hair1(x,y,r,g,b);
-let hair2 = new Hair2(x,y,r,g,b);
-// let hair3 = new Hair3(x,y,r,g,b);
-// let hair4 = new Hair4(x,y,r,g,b);
-// let hair5 = new Hair5(x,y,r,g,b);
-
+let hairImg =[];
+let hairClicked = false;
 
 /**
 Description of preload
 */
 function preload() {
-  for (let i=0; i< hairstylesNum; i++){
+  // for (let i=0; i< hairstylesNum; i++){
 // how to assign an image to a hairstyle? do we call it in the class or script?
-hair1img = loadImage('assets/images/hair1.jpg');
-hair2img = loadImage('assets/images/hair2.jpeg');
-hair3img = loadImage('assets/images/hair3.png');
-hair4img = loadImage('assets/images/hair4.png');
-hair5img = loadImage('assets/images/hair5.png');
-}
+hairImg.push (loadImage('assets/images/hair1.png'));
+hairImg.push (loadImage('assets/images/hair2.png'));
+hairImg.push (loadImage('assets/images/hair3.png'));
+hairImg.push (loadImage('assets/images/hair4.png'));
+hairImg.push (loadImage('assets/images/hair5.png'));
+
 }
 
 /**
@@ -48,8 +43,15 @@ Description of setup
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-for (let i=0; i< hairstylesNum; i++){
-}
+// for (let i=0; i< hairstylesNum; i++){
+// }
+
+// calling the img
+hairstyles.push (new Hairstyles(600,100,hairImg[0]));
+hairstyles.push (new Hairstyles(600,200,hairImg[1]));
+hairstyles.push (new Hairstyles(600,300,hairImg[2]));
+hairstyles.push (new Hairstyles(600,400,hairImg[3]));
+hairstyles.push (new Hairstyles(600,500,hairImg[4]));
 
 }
 
@@ -60,26 +62,11 @@ Description of draw()
 function draw() {
   background(0);
 displayModel();
-displayHipBox();
-// then how do we activate the functions like display, etc?
-displayHair();
-displayHair2();
-// mousePressed();
-if(mouseIsPressed){
-  let d= dist(mouseX, mouseY, hair1.x,hair1.y)
-  console.log("mouse is being pressed")
-  if (d < hair1.width){
-  console.log("hair1 is being pressed")
-    hair1.x= mouseX;
-    hair1.y= mouseY;
-  }
-  let f= dist(mouseX, mouseY, hair2.x,hair2.y)
-  console.log("mouse is being pressed")
-  if (d < hair2.width){
-  console.log("hair2 is being pressed")
-    hair2.x= mouseX;
-    hair2.y= mouseY;
-  }
+// displayHipBox();
+
+for (let i=0; i< hairstyles.length; i++){
+  hairstyles[i].displayHair();
+hairstyles[i].drag();
 }
 
 } //end draw
@@ -98,5 +85,31 @@ function displayHipBox(){
   stroke(255,0,0);
   strokeWeight(4)
   rect(hipBox.x,hipBox.y,hipBox.width,hipBox.height);
+  pop();
+}
+
+function mouseReleased (){
+  console.log(this.monoClick);
+    for (let i=0; i< hairstyles.length; i++){
+      console.log(i,hairstyles[i].monoClick);
+      hairstyles[i].monoClick = false;
+}
+  hairClicked = false;
+}
+
+function reset(){
+  hairstyles=[];
+  hairstyles.push (new Hairstyles(600,100,hairImg[0]));
+  hairstyles.push (new Hairstyles(600,200,hairImg[1]));
+  hairstyles.push (new Hairstyles(600,300,hairImg[2]));
+  hairstyles.push (new Hairstyles(600,400,hairImg[3]));
+  hairstyles.push (new Hairstyles(600,500,hairImg[4]));
+}
+
+function resetButton(){
+  push();
+  fill(0);
+  noStroke();
+  ellipse()
   pop();
 }
